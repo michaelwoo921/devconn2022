59db7c016e3cc94b451e37ec6d28a0dbc6946741
Jung Woo <michaelwoo921@gmail.com>
2022-04-10 00:23:45 -0700

    create, get, update, delete profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,150 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const { check, validationResult } = require('express-validator');
+
+// @route GET api/profile/me
+// @desc Get current users profile
+// @access Private
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.status(200).json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route POST api/profile
+// @desc Create Profile
+// @access Private
+
+router.post(
+  '/',
+  auth,
+  check('status').notEmpty().withMessage('Status is required'),
+  check('skills').notEmpty().withMessage('Skills is required'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      skills,
+      githubusername,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (skills)
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    if (bio) profileFields.bio = bio;
+    if (githubusername) profileFields.githubusername = githubusername;
+    const socialFields = {};
+    if (youtube) socialFields.youtube = youtube;
+    if (twitter) socialFields.twitter = twitter;
+    if (facebook) socialFields.facebook = facebook;
+    if (linkedin) socialFields.linkedin = linkedin;
+    if (instagram) socialFields.instagram = instagram;
+    profileFields.social = socialFields;
+
+    try {
+      console.log(profileFields);
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route GET api/profile
-// @desc Test route
+// @desc GET All Profiles
 // @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find({}).populate('user', [
+      'name',
+      'avatar',
+    ]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
-router.get('/', (req, res) => res.send('Profile route'));
+// @route GET api/profile/user/:user_id
+// @desc GET Profile by user ID
+// @access Public
+router.get('/user/:user_id', async (req, res) => {
+  console.log(req.params.user_id);
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) return res.status(400).json({ msg: 'No profile found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'No profile found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route DELETE api/profile
+// @desc DELETE Profile, user and posts
+// @access Private
+router.delete('/user/:user_id', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({
+      user: req.user.id,
+    });
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
